5e147d0f2545606e27743e783277c6a9179e7847
Mirsandev <xmirandadevx@gmail.com>
2023-12-22 16:18:17 -0400

    autoload, composer, db query

--- 09.php
+++ 09.php
@@ -1,4 +1,27 @@
 <?php include 'includes/header.php';
 
+// Conectar a la BD con Mysqli
+
+$db = new mysqli('localhost', 'root', '', 'bienesraices_inicio');
+
+// Creamos el query
+$query = "SELECT titulo, imagen from propiedades";
+
+// Lo preparamos
+$stmt = $db->prepare($query);
+
+// lo ejecutamos 
+$stmt->execute();
+
+// Creaos la variable 
+$stmt->bind_result($titulo, $imagen);
+
+// Asignamos el resultado
+
+
+// Imprimir el resultado
+while($stmt->fetch()):
+  var_dump($titulo);
+endwhile;
 
 include 'includes/footer.php';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mery/introduccion-phpoop_inicio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '76ba32234ad0c40b1259437f8d23ffcc19914465',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'a49db6f0a5033aef5143295342f1c95521b075ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mery/introduccion-phpoop_inicio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '76ba32234ad0c40b1259437f8d23ffcc19914465',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
